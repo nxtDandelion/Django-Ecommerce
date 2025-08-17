5457d6e59260848c4025c19a18e74771dfdf63c2
nxtDandelion <stanislavkasmak6@gmail.com>
2025-08-17 17:16:43 +0300

    Add seller, category, product models

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.db.models import ForeignKey
+from autoslug import AutoSlugField
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.common.models import BaseModel
+
+
+class Seller(BaseModel):
+    """
+    Seller model
+
+    Attributes:
+        General:
+            user (ForeignKey): User model
+            company_name (CharField): Company name
+            slug (AutoSlugField): Slug
+            inn_identifier (CharField): Seller INN identifier
+            website_url (URLField): Seller's website URL
+            phone_number (CharField): Seller phone number
+            company_description (CharField): Seller company description
+
+        Company location:
+            company_address (CharField): Seller company address
+            city (CharField): Seller city
+            postal_code (CharField): Seller postal code
+
+        Bank info:
+            bank_name (CharField): Seller bank name
+            bank_bic_number (CharField): Seller bank bic number
+            bank_account_number (CharField): Seller bank account number
+    """
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
+
+    company_name = models.CharField(max_length=120)
+    slug = AutoSlugField(populate_from='company_name', always_update=True, null=True, unique=True)
+    inn_identifier = models.CharField(max_length=50)
+    website_url = models.URLField(null=True, blank=True)
+    phone_number = models.CharField(max_length=11)
+    company_description = models.TextField()
+
+    company_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    bank_name = models.CharField(max_length=120)
+    bank_bic_number = models.CharField(max_length=9)
+    bank_account_number = models.CharField(max_length=100)
+
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.company_name}"
--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from autoslug import AutoSlugField
+from django.db.models import ImageField
 
-# Create your models here.
+from apps.common.models import BaseModel, IsDeletedModel
+from apps.sellers.models import Seller
+
+
+class Category(BaseModel):
+    """
+    Category model
+
+    Attributes:
+        name (CharField): Category name, unique for each instance
+        slug (CharField): Category slug, used in URL's
+        image (ImageField): Category image
+    """
+    name = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True, always_update=True)
+    image = ImageField(upload_to='category_images/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Product(IsDeletedModel):
+
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='products', null=True)
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from='name', unique=True, db_index=True)
+    description = models.TextField()
+    price_old = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    price_current = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    in_stock = models.IntegerField(default=5)
+
+    image1 = models.ImageField(upload_to='product_images/')
+    image2 = models.ImageField(upload_to='product_images/', blank=True)
+    image3 = models.ImageField(upload_to='product_images/', blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
